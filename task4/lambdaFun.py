f320e5ecfb9f6710e10d36ce325fabc28f4cba02
Mohsin Khan <ranamohsin4520@email.com>
2025-07-18 20:42:05 -0400

    Updated  lambdaFun.py

--- task4/lambdaFun.py
+++ task4/lambdaFun.py
@@ -1,3 +1,24 @@
 nums=[2,3,4,5,6,7,8,9,15,14]
 my_num=list(filter(lambda x:x%2==0,nums))
 print("These are Even numbs:",my_num)
+
+
+
+
+# decoraters
+def log_deco(fun):
+
+    def wrapper():
+
+        print("Before Function calling")
+        fun()  
+
+        print("After Function Execition") 
+
+@log_deco
+def hello():
+    print("Execution completed")
+
+hello()    
+
+
